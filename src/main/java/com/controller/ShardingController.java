de0e70425934f3f0fb04ead748eb0747298d55ad
dzb <290059121@qq.com>
2022-01-14 15:47:36 +0800

    test

--- src/main/java/com/controller/ShardingController.java
+++ src/main/java/com/controller/ShardingController.java
@@ -22,10 +22,15 @@ public class ShardingController {
         for (int i = 0; i < 10; i++) {
             Goods good = new Goods();
             good.setGname("小米手机");
-            good.setUserId(current.nextLong(1, 100));
+            good.setUserId(current.nextLong(100, 1000));
             good.setGstatus("已发布");
             goodsMapper.insert(good);
         }
         return "success";
+    }
+    @GetMapping("/hello2")
+    public Goods hello2() {
+        Goods goods = goodsMapper.selectById(1481857195800866818L);
+        return goods;
     }
 }
